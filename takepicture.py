e3f2082fa6af37c78411b515b7e079388fad6da4
studentAutomations <student.automations@gmail.com>
2024-11-22 00:38:34 +0100

    Update takepicture.py

--- takepicture.py
+++ takepicture.py
@@ -20,37 +20,9 @@ page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options
 
 try:
     # Navigate to the page and perform login actions
-    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/")
-    page_to_scrape.find_element(By.LINK_TEXT, "Log in").click()
-    time.sleep(5)
+    page_to_scrape.get("https://sip.elfak.ni.ac.rs/")
 
-    page_to_scrape.find_element(By.LINK_TEXT, "OpenID Connect").click()
-    time.sleep(5)
-
-    mail = page_to_scrape.find_element(By.ID, "i0116")
-    mail.send_keys(os.environ['MAIL'])  # Use environment variable for email
-    page_to_scrape.find_element(By.ID, "idSIButton9").click()
-    time.sleep(5)
-
-    password = page_to_scrape.find_element(By.ID, "i0118")
-    password.send_keys(os.environ['PASSWORD'])  # Use environment variable for password
-    page_to_scrape.find_element(By.ID, "idSIButton9").click()
-    time.sleep(5)
-
-    page_to_scrape.find_element(By.ID, "idBtn_Back").click()
-    time.sleep(5)
-
-    page_to_scrape.find_element(By.LINK_TEXT, "Baze").click()
-    time.sleep(5)
-
-    page_to_scrape.find_element(By.CLASS_NAME, "instancename").click()
-    time.sleep(5)
-
-    page_to_scrape.find_element(By.LINK_TEXT, "News forum").click()
-    time.sleep(5)
-
-
-    responseT = page_to_scrape.find_element(By.ID, "region-main")
+    responseT = page_to_scrape.find_element(By.ID, "novosti")
     responseT.screenshot('1.png')
    
 
