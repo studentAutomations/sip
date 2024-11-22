50f1f2184b78487ec818586663fbd458a93ffcb3
studentAutomations <student.automations@gmail.com>
2024-11-22 16:25:26 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -23,10 +23,25 @@ try:
     page_to_scrape.get("https://sip.elfak.ni.ac.rs/")
 
 
-    time.sleep(30)
+    time.sleep(5)
     responseT = page_to_scrape.find_element(By.ID, "novosti")
+    
+    
+    # Get the size of the element
+    height = responseT.size['height']
+    width = responseT.size['width']
+
+    # Set a larger window size (you can adjust these values as needed)
+    # Here we set a fixed width that is larger than what might be needed.
+    desired_width = max(width + 100, 1200)  # Ensure at least 1200px width
+    page_to_scrape.set_window_size(desired_width, height + 100)  # Adding some extra space
+
+    # Scroll to make sure the element is visible in case it's off-screen
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
+
+    # Take a screenshot of the entire element
     responseT.screenshot('sip-nova-obavestenja.png')
-   
+  
 
 finally:
     # Clean up by quitting the driver
