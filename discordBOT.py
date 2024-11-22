f47a2683238481172b1521df542ed907e2c66a97
studentAutomations <student.automations@gmail.com>
2024-11-22 17:01:02 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -14,7 +14,7 @@ image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG79
 image2 = 'https://github.com/studentAutomations/sip/blob/main/sip-nova-obavestenja.png'
 
 embed.set_author(name='Nova obavestenja na Sip-u')
-embed.add_field(name='Idi na sajt - ', value='https://cs.elfak.ni.ac.rs/nastava/')
+embed.add_field(name='Idi na sajt - ', value='https://sip.elfak.ni.ac.rs/')
 embed.add_field(name='Vidi obavestenja - ', value=image2)
 embed.set_footer(text='Elektronski Fakultet')
 embed.set_thumbnail(image1)
