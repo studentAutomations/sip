84a108fbc44a80e408b01d5092a5269b5486edac
studentAutomations <student.automations@gmail.com>
2024-11-22 01:17:34 +0100

    Update discord.py

--- discord.py
+++ discord.py
@@ -3,20 +3,26 @@ from dhooks import Webhook, Embed
 
 # Get the webhook URL from environment variable
 WEBHOOK_URL = os.getenv('WEBHOOK_URL')
-hook = Webhook(WEBHOOK_URL) 
+hook = Webhook(WEBHOOK_URL)
 
+# Create an embed object for the webhook
 embed = Embed(
     color=0xFF4545,
     timestamp='now'
 )
 
+# Define your images
 image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG795QYLmYvJl6G6CcyKbKHA&s'
-image2 = '1.png'
+image2 = '1.png'  # Path to the local image file you want to send
 
+# Set up the embed fields and attributes
 embed.set_author(name='Studentski Informacioni Portal')
 embed.add_field(name='Nova obaveštenja', value='https://sip.elfak.ni.ac.rs/')
 embed.set_footer(text='Elektronski Fakultet')
 embed.set_thumbnail(image1)
 
+# Send the embed along with the image file
+with open(image2, 'rb') as f:  # Open the image file in binary mode
+    hook.send(embed=embed, file=f)  # Attach the local image file
 
-hook.send(embed=embed, file=image2)
+
