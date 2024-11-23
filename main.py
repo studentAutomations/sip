e60f2e4b31660a543c022e73c668761c0c4f15e7
studentAutomations <student.automations@gmail.com>
2024-11-23 14:32:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,31 @@ newText = html.find('section', {"id" : "novosti"})
 # create markdown string of courses
 novosti_markdown = newText.text
 
+
+
+
+
 # write markdown file
 with open("novosti.md", "w") as novosti_file:
     novosti_file.write(novosti_markdown)
+
+
+
+
+responseT = page_to_scrape.find_element(By.ID, "novosti")
+    
+    
+    # Get the size of the element
+    height = responseT.size['height']
+    width = responseT.size['width']
+
+    # Set a larger window size (you can adjust these values as needed)
+    # Here we set a fixed width that is larger than what might be needed.
+    desired_width = max(width + 100, 1200)  # Ensure at least 1200px width
+    page_to_scrape.set_window_size(desired_width, height + 100)  # Adding some extra space
+
+    # Scroll to make sure the element is visible in case it's off-screen
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
+
+    # Take a screenshot of the entire element
+    responseT.screenshot('sip-nova-obavestenja.png')
