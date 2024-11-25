6d337a67ce7591cc6c32e5ee953ca0338e949054
studentAutomations <student.automations@gmail.com>
2024-11-25 02:43:13 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -15,7 +15,7 @@ for url in WEBHOOK_URL:
     hook.send('@everyone Nova obavestenja na SIP-u!')
 
     image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG795QYLmYvJl6G6CcyKbKHA&s'
-    image2_path = 'path/to/sip-nova-obavestenja.png'  # Local path to the image
+    image2_path = 'https://github.com/studentAutomations/sip/blob/main/sip-nova-obavestenja.png'  # Local path to the image
 
     embed.add_field(name='Idi na sajt - ', value='https://cs.elfak.ni.ac.rs/nastava/')
     embed.add_field(name='Vidi obavestenja - ', value='Image attached below.')
