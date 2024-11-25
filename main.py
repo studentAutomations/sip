6dbca1bc1a672d78f89814bac9352955a0dbd4d3
studentAutomations <student.automations@gmail.com>
2024-11-25 23:19:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from bs4 import BeautifulSoup
 
 sip_url = "https://sip.elfak.ni.ac.rs/"
 
-# get website contents and parse html
 response = requests.get(sip_url)
 html = BeautifulSoup(response.text, 'html.parser')
 
 newText = html.find('section', {"id" : "novosti"})
 
-# create markdown string of courses
+
 novosti_markdown = newText.text
 
-# write markdown file
+
 with open("novosti.md", "w") as novosti_file:
     novosti_file.write(novosti_markdown)
