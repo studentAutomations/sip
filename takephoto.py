a6187d7a38af9f0e142208fe695dbfcc351055bf
studentAutomations <student.automations@gmail.com>
2024-11-25 03:16:37 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -27,14 +27,14 @@ try:
     responseT = page_to_scrape.find_element(By.ID, "novosti")
     
     
-       # Get the size of the element
+         # Get the size of the element
     height = responseT.size['height']
     width = responseT.size['width']
 
     # Set a larger window size (you can adjust these values as needed)
     # Here we set a fixed width that is larger than what might be needed.
-    
-    page_to_scrape.set_window_size(width, height)  # Adding some extra space
+    desired_width = max(width + 100, 1200)  # Ensure at least 1200px width
+    page_to_scrape.set_window_size(desired_width, height * 0.4)  # Adding some extra space
 
     # Scroll to make sure the element is visible in case it's off-screen
     page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
