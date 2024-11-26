e36222768b799ebbcf7513693ecdc8eac9166c12
studentAutomations <student.automations@gmail.com>
2024-11-26 10:28:37 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,23 +1,19 @@
 import os
-from dhooks import Webhook, Embed, File
+from discord import Webhook, RequestsWebhookAdapter, File
 
 
 WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN'), os.getenv('WEBHOOK_OTHER1')]
 
 for url in WEBHOOK_URL:
-    hook = Webhook(url)
+    hook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
 
 
-    hook.send('**@everyone Nova obavestenja na SIP-u!**')
+    message_content = '**@everyone Nova obavestenja na SIP-u!**\n\n**>>> https://sip.elfak.ni.ac.rs/**'
+    image_path = 'sip-nova-obavestenja.png'
 
-    
-    image2_path = 'sip-nova-obavestenja.png' 
 
-    
-   
-    hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'))
-
-    hook.send('**>>> https://sip.elfak.ni.ac.rs/**')
+    with open(image_path, 'rb') as image_file:
+    hook.send(content=message_content, file=File(image_file, name='sip-nova-obavestenja.png'))
 
 
 
