1e5dff76742f779560d42c6f3a6bd757c908380c
studentAutomations <student.automations@gmail.com>
2024-11-26 10:36:48 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -8,12 +8,20 @@ for url in WEBHOOK_URL:
     hook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
 
 
+    def send_message():
+    
     message_content = '**@everyone Nova obavestenja na SIP-u!**\n\n**>>> https://sip.elfak.ni.ac.rs/**'
     image_path = 'sip-nova-obavestenja.png'
 
+    
+    hook.send(content=message_content)
 
+    
     with open(image_path, 'rb') as image_file:
-    hook.send(content=message_content, file=File(image_file, name='sip-nova-obavestenja.png'))
+        hook.send(file=File(image_file, name='sip-nova-obavestenja.png'))
+
+
+    send_message()
 
 
 
