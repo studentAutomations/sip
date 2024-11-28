3892c95e90290132e5fe5140e4dac0e22a05887f
studentAutomations <student.automations@gmail.com>
2024-11-28 16:01:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import os
 
-sip_url = "https://sip.elfak.ni.ac.rs/"
+chrome_options = Options()
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/google-chrome"  # Updated for GitHub runner
 
-response = requests.get(sip_url)
-html = BeautifulSoup(response.text, 'html.parser')
+browser_driver = Service('/usr/bin/chromedriver')  # Updated for GitHub runner
 
-newText = html.find('section', {"id" : "novosti"})
+page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
+
+try:
+    page_to_scrape.get("https://sip.elfak.ni.ac.rs/")
+
+    responseT = page_to_scrape.find_element(By.ID, "novosti")
 
 
-novosti_markdown = newText.text
+    height = responseT.size['height']
+    width = responseT.size['width']     
+    desired_width = max(width, 1200)  
+    desired_height = min(height, 1000)
+    page_to_scrape.set_window_size(desired_width, desired_height)     
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)  
+    responseT.screenshot('sip-nova-obavestenja.png')
 
+    novosti_markdown = responseT.text
+    with open("novosti.md", "w") as novosti_file:
+        novosti_file.write(novosti_markdown)
 
-with open("novosti.md", "w") as novosti_file:
-    novosti_file.write(novosti_markdown)
+finally:
+    page_to_scrape.quit()
