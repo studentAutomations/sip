1fb99759ecdd871afba63ce2dec92ac1aab8956c
studentAutomations <student.automations@gmail.com>
2024-11-28 16:21:03 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,5 +1,6 @@
 import os
 from dhooks import Webhook, Embed, File
+import time
 
 WEBHOOK_URL = [os.getenv('PROBA')]
 for url in WEBHOOK_URL:
@@ -9,9 +10,17 @@ for url in WEBHOOK_URL:
    
     image2_path = 'sip-nova-obavestenja.png' 
 
-    hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'))
+    # Send the image
+    with File(image2_path, name='sip-nova-obavestenja.png') as file:
+        hook.send(file=file)
 
+    # Send the link
     hook.send('**>>> https://sip.elfak.ni.ac.rs/**')
 
-    if os.path.exists("sip-nova-obavestenja.png"):
-        os.remove("sip-nova-obavestenja.png")
+    # Ensure the file is deleted
+    if os.path.exists(image2_path):
+        time.sleep(1)  # Ensure file is not locked
+        os.remove(image2_path)
+        print("Screenshot deleted successfully.")
+    else:
+        print("Screenshot not found.")
