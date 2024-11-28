ddcbffb58ccebf87acdb1ddee1816dbbb1d54ed8
studentAutomations <student.automations@gmail.com>
2024-11-28 16:48:03 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,43 +1,14 @@
 import os
-import requests  # To download the file
-from dhooks import Webhook, File
-import time
+from dhooks import Webhook, Embed, File
 
-# Webhook URL
 WEBHOOK_URL = [os.getenv('PROBA')]
-
-# GitHub raw URL of the file
-github_raw_url = "https://raw.githubusercontent.com/studentAutomations/sip/main/sip-nova-obavestenja.png"
-local_file_path = "sip-nova-obavestenja.png"
-
-# Download the file locally
-response = requests.get(github_raw_url)
-if response.status_code == 200:
-    with open(local_file_path, "wb") as file:
-        file.write(response.content)
-    print(f"Downloaded file: {local_file_path}")
-else:
-    print(f"Failed to download the file. Status code: {response.status_code}")
-    exit(1)
-
-# Send the file using the webhook
 for url in WEBHOOK_URL:
-    hook = Webhook(url)
+    hook = Webhook(url) 
 
-    # Notify users
-    hook.send("**@everyone Nova obavestenja na SIP-u!**")
+    hook.send('**@everyone Nova obavestenja na SIP-u!**')
+   
+    image2_path = 'sip-nova-obavestenja.png' 
 
-    # Send the image
-    file = File(local_file_path, name="sip-nova-obavestenja.png")
-    hook.send(file=file)
+    hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'))
 
-    # Send the link to the website
-    hook.send("**>>> https://sip.elfak.ni.ac.rs/**")
-
-# Clean up: Delete the local file
-if os.path.exists(local_file_path):
-    time.sleep(1)  # Allow time for file handling to finish
-    os.remove(local_file_path)
-    print(f"Screenshot deleted successfully.")
-else:
-    print("Screenshot not found.")
+    hook.send('**>>> https://cs.elfak.ni.ac.rs/nastava/**')
