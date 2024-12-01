d1836e64db7a377f852cb12ad3ec3658ac138be6
studentAutomations <student.automations@gmail.com>
2024-12-01 21:30:31 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -5,7 +5,7 @@ WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN'), os.getenv('WEBHOOK_OTHER1')]
 for url in WEBHOOK_URL:
     hook = Webhook(url) 
 
-    hook.send('**@everyone SIP**')
+    hook.send('**@everyone**')
    
     image2_path = 'sip-nova-obavestenja.png' 
 
