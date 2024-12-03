955356098e8912d68e769ec2806b483e03a4c161
studentAutomations <student.automations@gmail.com>
2024-12-03 20:08:37 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -9,7 +9,7 @@ for url in WEBHOOK_URL:
 
     embed = Embed(
         description="**@everyone**\n\n>>> [**SIP link**](https://sip.elfak.ni.ac.rs/)",
-        color=0x3498DB
+        color=0xE74C3C
     )
     embed.set_image(url=f"attachment://{image2_path}")  
 
