a2969caaa3734a9bb4abae63ef200d1303db8ee3
studentAutomations <student.automations@gmail.com>
2024-12-03 19:54:07 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,28 +1,22 @@
-import random
+from itertools import cycle
 import os
 from dhooks import Webhook, Embed, File
 
 image2_path = 'sip-nova-obavestenja.png'
 
-def random_color():
-    return random.randint(0, 0xFFFFFF)
+colors = [0x3498db, 0xe74c3c, 0x2ecc71, 0x9b59b6]
+color_cycle = cycle(colors)
 
 WEBHOOK_URL = [os.getenv('PB')]
 for url in WEBHOOK_URL:
     hook = Webhook(url) 
 
-   # hook.send('**@everyone**')
-   
-      
-
-  #  hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'))
-
-  #  hook.send('**>>> https://sip.elfak.ni.ac.rs/**')
+   embed_color = next(color_cycle)
 
 
     embed = Embed(
-        description="**@everyone**\n\n>>> [SIP link](https://cs.elfak.ni.ac.rs/nastava/)",
-        color=0xFFFF00 
+        description="**@everyone**\n\n>>> [**SIP link**](https://cs.elfak.ni.ac.rs/nastava/)",
+        color=embed_color 
     )
     embed.set_image(url=f"attachment://{image2_path}")  
 
