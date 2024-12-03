f1275d212b73b24586bee1599cd1d41e3f1d8af5
studentAutomations <student.automations@gmail.com>
2024-12-03 19:25:35 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -5,10 +5,20 @@ WEBHOOK_URL = [os.getenv('PB')]
 for url in WEBHOOK_URL:
     hook = Webhook(url) 
 
-    hook.send('**@everyone**')
+   # hook.send('**@everyone**')
    
-    image2_path = 'sip-nova-obavestenja.png' 
+     image2_path = 'sip-nova-obavestenja.png' 
 
-    hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'))
+  #  hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'))
 
-    hook.send('**>>> https://sip.elfak.ni.ac.rs/**')
+  #  hook.send('**>>> https://sip.elfak.ni.ac.rs/**')
+
+
+    embed = Embed(
+        description="**@everyone**\n\n>>> [Kliknite ovde za detalje.](https://cs.elfak.ni.ac.rs/nastava/)",
+        color=0x3498db
+    )
+    embed.set_image(url=f"attachment://{image2_path}")  
+
+    
+    hook.send(file=File(image2_path, name='sip-nova-obavestenja.png'), embed=embed)
