7068e95d906b1b177a295b0cf2f9ccb158ae5200
studentAutomations <student.automations@gmail.com>
2024-12-11 17:57:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
 
 # Chrome options
 chrome_options = Options()
